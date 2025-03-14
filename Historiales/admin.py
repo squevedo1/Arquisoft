10574f6a48118220f132458c4f782c0e222fd539
Sebastian <s.quevedo1@uniandes.edu.co>
2025-03-13 22:39:48 -0500

    Eventos medicos - no salen en la historia clinica

--- Historiales/admin.py
+++ Historiales/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import HistoriaClinica, Paciente
+from .models import HistoriaClinica, Paciente, EventoMedico
 
 admin.site.register(Paciente)
 admin.site.register(HistoriaClinica)
+admin.site.register(EventoMedico)
--- Historiales/models.py
+++ Historiales/models.py
@@ -30,7 +30,6 @@ class Paciente(models.Model):
     def __str__(self):
         return self.nombre
 
-from django.db import models
 
 
 class HistoriaClinica(models.Model):
@@ -40,3 +39,22 @@ class HistoriaClinica(models.Model):
 
     def __str__(self):
         return f"Historia Clínica de {self.paciente.nombre}"
+
+
+
+class EventoMedico(models.Model):
+    EVENTO_CHOICES = [
+        ('Examen Medico', 'Examen Medico'),
+        ('Consulta', 'Consulta'),
+        ('Preescripcion', 'Preescripcion'),
+        ('Cirugia', 'Cirugia'),
+    ]
+
+    historia_clinica = models.ForeignKey(HistoriaClinica, on_delete=models.CASCADE, related_name='eventos_medicos')
+
+    Tipo_evento = models.CharField(max_length=50, choices=EVENTO_CHOICES)
+    fecha_y_hora_del_evento = models.DateTimeField()
+    especificaciones = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.Tipo_evento} - {self.fecha_y_hora_del_evento}"
