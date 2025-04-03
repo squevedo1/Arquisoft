07cda14a44a8c0c4b5a4545f45a614dec3ffd071
Juan David Perez <jd.perezd1@uniandes.edu.co>
2025-04-03 09:15:35 -0500

    edicion url views, borrado sqlite

--- Arquisoft/urls.py
+++ Arquisoft/urls.py
@@ -19,5 +19,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home),
     path('health-check/', views.healthCheck)
 ]
--- Arquisoft/views.py
+++ Arquisoft/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse("Hello world! Django views")
+
 def index(request):
     return render(request, 'index.html')
 
