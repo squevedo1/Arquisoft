c6c16be30ad0c65bd4e41fa303297544f018ba1f
Juan David Perez <jd.perezd1@uniandes.edu.co>
2025-04-03 18:13:30 -0500

    creo que se va a dañar xd

--- evento_clinico/logic/evento_clinico_logic.py
+++ evento_clinico/logic/evento_clinico_logic.py
@@ -1,4 +1,7 @@
+import datetime
+from historia_clinica.models import HistoriaClinica
 from ..models import EventoMedico
+from django.core.exceptions import ObjectDoesNotExist
 
 def get_eventos_clinicos():
     eventos = EventoMedico.objects.all()
@@ -15,7 +18,12 @@ def update_evento_clinico(evento_pk, evento_var):
     return evento
 
 def create_evento_clinico(var):
-    evento = EventoMedico(historia_clinica=var["historia_clinica"], Tipo_evento=var["Tipo_evento"],
-                          fecha_y_hora_del_evento=var["fecha_y_hora_del_evento"], especificaciones=var["especificaciones"])
+    try:
+        historia_clinical = HistoriaClinica.objects.get(pk=var["historia_clinica"])
+    except ObjectDoesNotExist:
+        raise ValueError(f"No se encontró una historia clínica con el ID {var['historia_clinica']}")
+    evento = EventoMedico(historia_clinica=historia_clinical, Tipo_evento=var["Tipo_evento"],
+                          fecha_y_hora_del_evento=datetime.strptime(var["fecha_y_hora_del_evento"]), 
+                          especificaciones=var["especificaciones"])
     evento.save()
     return evento
