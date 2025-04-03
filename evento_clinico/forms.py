d61e6951022c58ef37fdc377f76b6b19845d24d0
Juan David Perez <jd.perezd1@uniandes.edu.co>
2025-04-03 18:05:41 -0500

    probando123

--- evento_clinico/forms.py
+++ evento_clinico/forms.py
@@ -4,15 +4,4 @@ from .models import EventoMedico
 class EventoClinicoForm(forms.ModelForm):
     class Meta:
         model = EventoMedico
-        fields = [
-            'historia_clinica',
-            'Tipo_evento',
-            'fecha_y_hora_del_evento',
-            'especificaciones'
-        ]
-        labels = {
-            'historia_clinica': 'Historia',
-            'Tipo_evento': 'Tipo_evento',
-            'fecha_y_hora_del_evento': 'fecha_y_hora_del_evento',
-            'especificaciones': 'especificaciones'
-        }
+        fields = '__all__'
