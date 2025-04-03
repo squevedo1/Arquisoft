027101701d534364d4c00228489836ac9ff3e899
Juan David Perez <jd.perezd1@uniandes.edu.co>
2025-04-03 11:37:20 -0500

    vista eventos medicso

--- evento_clinico/urls.py
+++ evento_clinico/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.eventos_clinicos_view, name='eventos_clinicos_view'),
+    path('eventos_clinicos/', views.eventos_clinicos_view, name='eventos_clinicos_view'),
     path('<int:pk>', views.evento_clinico_view, name='evento_clinico_view')
 ]
