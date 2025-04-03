62c4172907a6988f76493846218e82f25434561c
Juan David Perez <jd.perezd1@uniandes.edu.co>
2025-04-03 06:31:45 -0500

    cambios settings

--- Arquisoft/settings.py
+++ Arquisoft/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'Arquisoft.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+		'NAME': 'principal_db',
+		'USER': 'admin',
+		'PASSWORD': '12345',
+		'HOST': '10.128.0.2',
+		'PORT': '5432'
+	}
 }
 
 
