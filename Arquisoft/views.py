adab530208df2a9bfc1007b62bfc5539abbd009a
Juan David Perez <jd.perezd1@uniandes.edu.co>
2025-04-03 11:10:51 -0500

    probando algo

--- Arquisoft/views.py
+++ Arquisoft/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello world! Django views")
+    return render(request, 'index.html')
 
 def index(request):
     return render(request, 'index.html')
