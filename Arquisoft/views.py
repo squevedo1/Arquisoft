1bae89fb99357d827b96d2d77af48c510d5e7807
Juan David Perez <jd.perezd1@uniandes.edu.co>
2025-04-03 11:21:41 -0500

    reversando

--- Arquisoft/views.py
+++ Arquisoft/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, './templates/index.html')
+    return render(request, 'index.html')
 
 def healthCheck(request):
     return HttpResponse('ok')
