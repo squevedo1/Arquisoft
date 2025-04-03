0d988c7d4837b52b9b001faf18d697e70bed6a88
Juan David Perez <jd.perezd1@uniandes.edu.co>
2025-04-03 11:34:04 -0500

    settings.py

--- Arquisoft/settings.py
+++ Arquisoft/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'Arquisoft.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'Arquisoft', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
